cda94bb78d99bf0b19c177f63b4496f6455955ae
Andres Hernandez <edanhebla1213@hotmail.com>
2022-07-07 23:06:56 -0500

    jest config files

--- jest.config.itest.ts
+++ jest.config.itest.ts
@@ -1,12 +1,11 @@
 import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
-  roots: ['<rootDir>/src/test'],
+  roots: ['<rootDir>/src/itest'],
   preset: 'ts-jest',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   verbose: true,
-  collectCoverage: false,
-  collectCoverageFrom: ['<rootDir>/src/app/**/*.ts'],
+  testEnvironment: 'node',
 };
 
 export default config;
